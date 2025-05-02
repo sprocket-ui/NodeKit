313dea7bba46af12abfa9985db9adfe56509e1d4
Botond Szabo <112097901+botond-szabo@users.noreply.github.com>
2025-05-01 18:10:57 -0700

    Updated branch code to latest

--- packages/@sprocketui-react/sprocketui-react-button/src/index.ts
+++ packages/@sprocketui-react/sprocketui-react-button/src/index.ts
@@ -8,5 +8,22 @@
 
 'use strict';
 
-export { useButton, type ButtonHookProps, type ButtonHookResult } from './hooks/useButton';
-export { Button, ButtonContext, type ButtonProps } from './components/Button';
+export {
+  useButton,
+  useButton as useSprocketButton
+} from './hooks/useButton';
+export type {
+  ButtonHookProps,
+  ButtonHookResult
+} from './hooks/useButton';
+
+export {
+  Button,
+  ButtonContext,
+  Button as SprocketButton,
+  ButtonContext as SprocketButtonContext
+} from './components/Button';
+export type {
+  ButtonProps,
+  ButtonProps as SprocketButtonProps
+} from './components/Button';
