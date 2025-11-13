0332d5e57e3a187cc1ac1a6ec44d9a2e8b9267ae
botond-szabo <112097901+botond-szabo@users.noreply.github.com>
2025-11-12 22:42:02 -0700

    Added new react Input component

---
+++ packages/@sprocketui-react/sprocketui-react-input/src/components/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Corinvo, LLC. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+export {
+  Input,
+  Input as SprocketInput
+} from './Input';
+
+export type {
+  InputProps,
+  InputProps as SprocketInputProps
+} from './Input.types';
